5d29b7d8cc260f1978f2cd4976208e04bf962d98
GibbedHead <chaplygin-ma@yandex.ru>
2024-10-23 17:27:20 +0300

    Подправил реализацию

--- src/main/java/ru/chaplyginma/circularbuffer/CircularBuffer.java
+++ src/main/java/ru/chaplyginma/circularbuffer/CircularBuffer.java
@@ -1,11 +1,17 @@
 package ru.chaplyginma.circularbuffer;
 
+import java.util.Arrays;
+
 public class CircularBuffer<T> {
 
     private final Object[] elements;
     private final int capacity;
-    private int read = -1;
+    private boolean overwrite = false;
+
+    private int read = 0;
     private int write = 0;
+    private boolean full = false;
+
 
     public CircularBuffer(int capacity) {
         if (capacity < 1) {
@@ -13,15 +19,53 @@ public class CircularBuffer<T> {
         }
         this.capacity = capacity;
         elements = new Object[capacity];
-
     }
 
-    public void add(T element) {
-        elements[write++ % capacity] = element;
+    public CircularBuffer(int capacity, boolean overwrite) {
+        this(capacity);
+        this.overwrite = overwrite;
+    }
+
+    public boolean add(T element) {
+        if (!overwrite && isFull()) {
+            return false;
+        }
+        elements[write] = element;
+        write = (write + 1) % capacity;
+        if (write == read) {
+            full = true;
+        }
+        return true;
     }
 
     @SuppressWarnings("unchecked")
     public T get() {
-        return (T) elements[++read];
+        if (isEmpty()) {
+            return null;
+        }
+        T element = (T) elements[read];
+        elements[read] = null;
+        read = (read + 1) % capacity;
+        if (read == write) {
+            full = false;
+        }
+        return element;
+    }
+
+    private boolean isEmpty() {
+        return read == write && !full;
+    }
+
+    private boolean isFull() {
+        return read == write && full;
+    }
+
+    @Override
+    public String toString() {
+        return "CircularBuffer{" +
+                "elements=" + Arrays.toString(elements) +
+                ", read=" + read +
+                ", write=" + write +
+                '}';
     }
 }
